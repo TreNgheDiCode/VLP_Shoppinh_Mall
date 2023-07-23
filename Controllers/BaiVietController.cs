4128398484f9012ffcf115a4b7e54873448a8019
TreNgheDiCode <gabayan170@gmail.com>
2023-07-23 22:49:51 +0700

    Update Views

---
+++ Controllers/BaiVietController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VLPMall.Controllers
+{
+    public class BaiVietController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("IndexBaiViet");
+        }
+    }
+}
---
+++ Controllers/TuyenDungController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VLPMall.Controllers
+{
+    public class TuyenDungController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("IndexTuyenDung");
+        }
+    }
+}
